59efc7929905a5f81979ae5ce4f7798128f56561
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 10:36:57 +0500

    binded countdown to a button

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-
+def start_timer():
+    count_down(5)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
     canvas.itemconfig(timer_text, text=count)
@@ -32,12 +33,12 @@ tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(row=1, column=1)
-count_down(5)
 
-start_button = Button(text='Start')
+
+start_button = Button(text='Start', command=start_timer, highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text='Reset')
+reset_button = Button(text='Reset', highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(text='✔', fg=GREEN, bg=YELLOW, font=(None, 20, 'bold'))
