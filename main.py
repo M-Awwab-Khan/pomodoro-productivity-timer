69308df4656ae4a9dada7809131f09e003cba557
Awwab <mawwabkhank2006@gmail.com>
2024-02-06 10:26:15 +0500

    designed user interface

--- main.py
+++ main.py
@@ -21,12 +21,23 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_label = Label(text='Timer', fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(row=0, column=1)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.grid(row=1, column=1)
 
+start_button = Button(text='Start')
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text='Reset')
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text='✔', fg=GREEN, bg=YELLOW, font=(None, 20, 'bold'))
+check_marks.grid(column=1, row=3)
 
 
 window.mainloop()
